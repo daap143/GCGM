c3034d3fcbf43264ff8fc6447dd9fb1ff4baaffd
daap143 <mayankdeep4080@gmail.com>
2024-01-12 20:07:46 +0530

    i made a banner

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 #imports
+from app import git , graph
 
 #banner
 
+banner = '''
+     GGGG      CCCC     GGGG    MM       MM
+    GG  GG    CC  CC   GG  GG   MMMM   MMMM
+   GG        CC    C  GG        MM MM MM MM
+   G         C        G         MM  MMM  MM
+   GG  GGG   CC       GG  GGG   MM   M   MM
+   GG    GG  CC    C  GG    GG  MM       MM
+    GG   GG   CC  CC   GG   GG  MM       MM
+     GGGGG     CCCC     GGGGG   MM       MM
+'''
+print(banner,"\nGitHub Contribution Graph Modifyre/Manipulator \n           by @daap143 (MAYANK DEEP) \n \n NOTE: Read the README file here or \n at https://www.github.com/daap143/GCGM/README.md \n or you can simply watch the tutorial on https://www.youtube.com/.... ")
 
 #input repo_url and usr/token/email
 
