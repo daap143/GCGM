45b8f566b6dec4fdc88662908e0341df67cfd59f
daap143 <mayankdeep4080@gmail.com>
2024-01-15 20:34:36 +0530

    the extract function is almost done for those who are thinking why i have took days just to create empty file , i din't.

--- app/git.py
+++ app/git.py
@@ -1,8 +1,13 @@
 #imports
+from os import system
 
 #def clone
-    #extract function
+def clone(repo_url):
+    #extract dir name function
+    i = len(repo_url)
+    extract(i,repo_url)
     #clone cmd
+    #system(f"git clone {repo_url}")
 
 #def commit
 
@@ -11,3 +16,19 @@
 
 
 #def extract
+def extract(i,repo_url):
+    temp = []
+    while(repo_url[i-1]!="/"):
+        temp.append(repo_url[i-1])
+        i-=1
+    repo = put(temp)
+    print(repo)
+
+def put(temp):
+    j = len(temp) - 1
+    string = ""
+    while(j>=0):
+        string = string + temp[j]
+        j-=1
+    return string
+
